a3e6544975b98cd9cd3d813507d0039e849c9bd4
Tomas <tomasbhmg@gmail.com>
2024-12-31 13:40:43 -0300

    menu e fucao para ler o csv

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,53 @@
+//imports
+import fs from 'fs';
+import csv from 'csv-parser';
+import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 
 const input = require('prompt-sync')({sigint: true});
-const nome = input('Digite seu nome: ');
-console.log("Olá ", nome);
+interface Dados{
+    nome: string;
+    peso: number;
+    valor: number;
+    quantidade: number;
+}
+//funçoes
+const readCSV = async (filePath: string): Promise<Dados[]> => {
+    return new Promise((resolve, reject) => {
+      const results: Dados[] = [];
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data: Dados) => results.push(data))
+        .on('end', () => resolve(results))
+        .on('error', (error) => reject(error));
+    });
+};
+
+const writeCSV = async (filePath: string, data: Dados[]): Promise<void> => {
+    const csvWriter = createCsvWriter({
+        path: filePath,
+        header: [
+          { id: 'nome', title: 'NOME' },
+          { id: 'peso', title: 'PESO' },
+          { id: 'valor', title: 'VALOR' },
+          { id: 'quantidade', title: 'QUANTIDADE' },
+        ],
+    });
+  
+    return csvWriter.writeRecords(data);
+};
+
+
+//menu
+console.log("Bem vindo\n1: Adicionar Produto\n2: Remover Produto\n3: Ver Estoque");
+var entrada = input('Digite a operacao: ');
+if(entrada == '3'){
+   const lerCSV = async () => {
+    try {
+        const dadosLidos = await readCSV('./estoque.csv');
+        console.log(dadosLidos);
+    } catch (error) {
+        console.log('erro: ',error);
+    }
+   }
+   lerCSV();
+}
