56a2c67819cc5276e65451c54d2258060a958ded
Tomas <tomasbhmg@gmail.com>
2025-01-03 16:01:12 -0300

    funcão para escrever no csv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 //imports
 import fs from 'fs';
 import csv from 'csv-parser';
-import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 
 const input = require('prompt-sync')({sigint: true});
+
 interface Dados{
     nome: string;
     peso: number;
@@ -24,30 +24,65 @@ const readCSV = async (filePath: string): Promise<Dados[]> => {
 const lerCSV = async () => {
     try {
         const dadosLidos = await readCSV('./estoque.csv');
-        console.log("Nome | Peso | Valor | Quantidade");
-        console.log(dadosLidos[0].nome, dadosLidos[0].peso, dadosLidos[0].valor, dadosLidos[0].quantidade);
+        if(dadosLidos.length == 0){
+            console.log("Não há produtos no estoque!");
+        } else {
+            console.log("Nome | Peso | Valor | Quantidade")
+            for(var i = 0; i < dadosLidos.length; i++){
+                console.log(dadosLidos[i].nome, dadosLidos[i].peso, dadosLidos[i].valor, dadosLidos[i].quantidade);
+            }
+        }
     } catch (error) {
         console.log('erro: ',error);
     }
-   }
+}
 
-const writeCSV = async (filePath: string, data: Dados[]): Promise<void> => {
-    const csvWriter = createCsvWriter({
-        path: filePath,
-        header: [
-          { id: 'nome', title: 'NOME' },
-          { id: 'peso', title: 'PESO' },
-          { id: 'valor', title: 'VALOR' },
-          { id: 'quantidade', title: 'QUANTIDADE' },
-        ],
-    });
-  
-    return csvWriter.writeRecords(data);
-};
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: './estoque.csv',
+    header: [
+        {id: 'nome', title: 'nome'},
+        {id: 'peso', title: 'peso'},
+        {id: 'valor', title: 'valor'},
+        {id: 'quantidade', title: 'quantidade'},
+    ]
+});
+
+async function escreverCSV() {
+    var nomeP = input('Digite o nome do produto: ')
+    var pesoP = input('Digite o peso do produto: ')
+    var valorP = input('Digite o valor do produto: ')
+    var quantP = input('Digite a quantidade do produto: ')
+
+    const dadosLidos = {
+        nome: nomeP,
+        peso: pesoP,
+        valor: valorP,
+        quantidade: quantP
+    } as Dados
+
+    var data = await readCSV('./estoque.csv');
+    data.push(dadosLidos);
+     
+    csvWriter.writeRecords(data)
+        .then(() => {
+            console.log('Produto adicionado no estoque com sucesso!');
+        });
+}
 
 //menu
-console.log("Bem vindo\n1: Adicionar Produto\n2: Remover Produto\n3: Ver Estoque");
-var entrada = input('Digite a operacao: ');
-if(entrada == '3'){
-   lerCSV();
-}
+do {
+    var entrada;
+    async function main() {
+        console.log('0: Encerrar Programa\n1: Ver Estoque\n2: Adicionar Produto')
+        entrada = input('Digite a operação: ');
+
+        if(entrada == '1'){
+            await lerCSV();
+        }
+        if(entrada == '2'){
+            await escreverCSV();  
+        }
+    }
+    main();
+} while (entrada !== '0');
