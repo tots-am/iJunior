6a532332bfdad14bec2b6c561a2e661df309f41f
Tomas <tomasbhmg@gmail.com>
2025-01-04 19:31:49 -0300

    rename controller

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
+/*
+IMPORTANTE!
+>> Olá avaliador, espero que esteja tudo bem com você!
+>> Peço ao senhor(a) que avalie esta atividade com o coração aberto, uma vez que esse é meu
+>> primeiro projeto com TS e JS e com qualquer linguagem orientada a Objetos.
+>> E é tambem por essa razão que gostaria de agradecer a vocês por essa oportunidade de passar por
+>> esse processo de aprendizagem, no qual eu superei desafios e obstáculos que achei que seriam
+>> impensáveis para mim a apenas algumas semanas atras.
+>> Obrigado. ;)
+*/
 //imports
-import { lerCSV } from './controller/funcoesEstoque';
-import { removerCSV } from './controller/funcoesEstoque';
-import { escreverCSV } from './controller/funcoesEstoque';
+import { lerCSV } from './service/funcoesEstoque';
+import { removerCSV } from './service/funcoesEstoque';
+import { escreverCSV } from './service/funcoesEstoque';
 
 const input = require('prompt-sync')({sigint: true});
 
 //menu
 var entrada;
-async function main() {
+const main = async() => {
     console.log('\n0: Encerrar Programa\n1: Adicionar Produto\n2: Remover Produto\n3: Ver Estoque e seus dados')
     entrada = input('Digite a operação: ');
     
@@ -16,14 +26,16 @@ async function main() {
             console.log('Programa Encerrado')
             break;
         case '1':
-            await escreverCSV();
+            await escreverCSV()
             break;
         case '2':
-            await removerCSV();
+            await removerCSV()
             break;
         case '3':
-            await lerCSV();
+            await lerCSV()
             break;
     }
 }
-main();
+while (entrada != 0) {
+    main();
+}
